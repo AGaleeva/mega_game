c2ac9f9ed87e5ee144322a2078863944b0d12446
AGaleea <a_galeeva@hotmail.com>
2023-11-19 16:18:16 +0100

    roadMines added

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // start mega-game project
+
+let roadMines = [true, true, true, true, true, true, true, true, true, true];
+
+let damaged = false;
+
+for (let i in roadMines) {
+  console.log(`танк переместился на ${parseInt(i) + 1}`);
+  if (roadMines[i] === true) {
+    if (damaged === false) {
+      damaged = true;
+      console.log("танк повреждён");
+    } else {
+      console.log("танк уничтожен");
+      break;
+    }
+  }
+}
